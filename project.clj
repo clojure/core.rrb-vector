55f0797ea687ced9fe663baafb3a3f762aff4533
Michał Marczyk <michal.marczyk@gmail.com>
2013-04-06 04:36:48 +0200

    Move to contrib directory structure, specify :parent in project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :url "https://github.com/clojure/core.rrb-vector"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :parent [org.clojure/pom.contrib "0.0.26"]
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
   :warn-on-reflection true)
