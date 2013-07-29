7a07c2d88ffc9dfb03319b96718cf39fddec1360
Michał Marczyk <michal.marczyk@gmail.com>
2013-07-29 10:13:35 +0200

    Silence Leiningen warning

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "https://github.com/clojure/core.rrb-vector"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.2.0"
   :parent [org.clojure/pom.contrib "0.1.2"]
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :source-paths ["src/main/clojure"
                  "src/main/cljs"]
   :test-paths ["src/test/clojure"]
-  :warn-on-reflection true
+  :global-vars {*warn-on-reflection* true}
   :jvm-opts ^:replace []
   :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-1847"]]
                    :plugins [[lein-cljsbuild "0.3.2"]]}}
