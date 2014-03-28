b1235d3d0c8d24088a73cb7ac36910b60e07c2d4
Michał Marczyk <michal.marczyk@gmail.com>
2014-03-28 02:51:02 +0100

    Bump 1.6 profile to 1.6.0

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                    :dependencies [[org.clojure/clojurescript "0.0-2156"]
                                   [collection-check "0.1.1"]]
                    :plugins [[lein-cljsbuild "1.0.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0-beta1"]]}}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :cljsbuild {:builds {:test {:source-paths ["src/main/cljs"
                                              "src/test/cljs"]
                               :compiler {:optimizations :advanced
