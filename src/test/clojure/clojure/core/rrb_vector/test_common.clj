4275db546cb0820d8864d3c56d58cdfd51b66ca4
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-09-18 14:13:15 -0700

    Move more tests into the common set run by both clj and cljs

--- src/test/clojure/clojure/core/rrb_vector/test_common.clj
+++ src/test/clojure/clojure/core/rrb_vector/test_common.clj
@@ -1,6 +1,8 @@
 (ns clojure.core.rrb-vector.test-common
-  (:require [clojure.test :as test :refer [deftest testing is]]
+  (:require [clojure.test :as test :refer [deftest testing is are]]
+            [clojure.core.reducers :as r]
             [clojure.core.rrb-vector :as fv]
+            [clojure.core.rrb-vector.debug :as dv]
             [clojure.core.rrb-vector.test-utils :as utils]))
 
 ;; The intent is to keep this file as close to
@@ -8,6 +10,130 @@
 ;; so that when we start requiring Clojure 1.7.0 and later for this
 ;; library, this file and that one can be replaced with a common test
 ;; file with the suffix .cljc
+
+(deftest test-slicing
+  (testing "slicing"
+    (is (dv/check-subvec 32000 10 29999 1234 18048 10123 10191))))
+
+;; TBD: Uncomment this test after I figure out which of my fixes gets
+;; it working again.
+#_(deftest test-splicing
+  (testing "splicing"
+    (is (dv/check-catvec 1025 1025 3245 1025 32768 1025 1025 10123 1025 1025))
+    (is (dv/check-catvec 10 40 40 40 40 40 40 40 40))
+    (is (apply dv/check-catvec (repeat 30 33)))
+    (is (dv/check-catvec 26091 31388 1098 43443 46195 4484 48099 7905
+                         13615 601 13878 250 10611 9271 53170))
+
+    ;; Order that catvec will perform splicev calls:
+    (let [my-catvec fv/catvec
+          ;; Consider switching to the next line if we add
+          ;; dv/dbg-splicev from branch into master.
+          ;;my-catvec dv/dbg-splicev
+          
+          counts [26091 31388 1098 43443 46195 4484 48099 7905
+                  13615 601 13878 250 10611 9271 53170]
+          
+          prefix-sums (reductions + counts)
+          ranges (map range (cons 0 prefix-sums) prefix-sums)
+          
+          [v01 v02 v03 v04 v05 v06 v07 v08
+           v09 v10 v11 v12 v13 v14 v15] (map fv/vec ranges)
+          
+          v01-02 (my-catvec v01 v02)  ;; top level catvec call
+          v03-04 (my-catvec v03 v04)  ;; top level catvec call
+          v01-04 (my-catvec v01-02 v03-04)  ;; top level catvec call
+          
+          v05-06 (my-catvec v05 v06)  ;; recurse level 1 catvec call
+          v07-08 (my-catvec v07 v08)  ;; recurse level 1 catvec call
+          v05-08 (my-catvec v05-06 v07-08)  ;; recurse level 1 catvec call
+          
+          v09-10 (my-catvec v09 v10)  ;; recurse level 2 catvec call
+          v11-12 (my-catvec v11 v12)  ;; recurse level 2 catvec call
+          v09-12 (my-catvec v09-10 v11-12)  ;; recurse level 2 catvec call
+
+          v13-14 (my-catvec v13 v14)  ;; recurse level 3 catvec call
+          v13-15 (my-catvec v13-14 v15)  ;; recurse level 3 catvec call
+
+          v09-15 (my-catvec v09-12 v13-15)  ;; recurse level 2 catvec call
+
+          v05-15 (my-catvec v05-08 v09-15)  ;; recurse level 1 catvec call
+
+          v01-15 (my-catvec v01-04 v05-15)  ;; top level catvec call
+
+          exp-val (range (last prefix-sums))]
+      (is (= -1 (dv/first-diff v01-15 exp-val)))
+      (is (= -1 (dv/first-diff (into v01-04 v05-15) exp-val))))))
+
+(deftest test-reduce
+  (let [v1 (vec (range 128))
+        v2 (fv/vec (range 128))]
+    (testing "reduce"
+      (is (= (reduce + v1) (reduce + v2))))
+    (testing "reduce-kv"
+      (is (= (reduce-kv + 0 v1) (reduce-kv + 0 v2))))))
+
+(deftest test-reduce-2
+  (let [v1 (fv/subvec (vec (range 1003)) 500)
+        v2 (vec (range 500 1003))]
+    (is (= (reduce + 0 v1)
+           (reduce + 0 v2)
+           (reduce + 0 (r/map identity (seq v1)))
+           (reduce + 0 (r/map identity (seq v2)))))))
+
+(deftest test-seq
+  (let [v (fv/vec (range 128))
+        s (seq v)]
+    (testing "seq contents"
+      (is (= v s)))
+    (testing "chunked-seq?"
+      (is (chunked-seq? s)))
+    (testing "internal-reduce"
+      (is (satisfies? clojure.core.protocols/InternalReduce
+                      s)))))
+
+(deftest test-assoc
+  (let [v1 (fv/vec (range 40000))
+        v2 (reduce (fn [out [k v]]
+                     (assoc out k v))
+                   (assoc v1 40000 :foo)
+                   (map-indexed vector (rseq v1)))]
+    (is (= (concat (rseq v1) [:foo]) v2)))
+  (are [i] (= :foo
+              (-> (range 40000)
+                  (fv/vec)
+                  (fv/subvec i)
+                  (assoc 10 :foo)
+                  (nth 10)))
+       1 32 1024 32768))
+
+(deftest test-assoc!
+  (let [v1 (fv/vec (range 40000))
+        v2 (persistent!
+            (reduce (fn [out [k v]]
+                      (assoc! out k v))
+                    (assoc! (transient v1) 40000 :foo)
+                    (map-indexed vector (rseq v1))))]
+    (is (= (concat (rseq v1) [:foo]) v2)))
+  (are [i] (= :foo
+              (-> (range 40000)
+                  (fv/vec)
+                  (fv/subvec i)
+                  (transient)
+                  (assoc! 10 :foo)
+                  (persistent!)
+                  (nth 10)))
+       1 32 1024 32768))
+
+(defn slow-into [to from]
+  (reduce conj to from))
+
+(deftest test-relaxed
+  (is (= (into (fv/catvec (vec (range 123)) (vec (range 68))) (range 64))
+         (concat (range 123) (range 68) (range 64))))
+  (is (= (slow-into (fv/catvec (vec (range 123)) (vec (range 68)))
+                    (range 64))
+         (concat (range 123) (range 68) (range 64)))))
 
 (deftest test-hasheq
   (is (= (hash []) (hash (fv/vector))))  ;; CRRBV-25
@@ -21,6 +147,88 @@
     (is (= (hash (nthnext s1 120))
            (hash (nthnext s2 120))
            (hash (nthnext s3 120))))))
+
+(deftest test-reduce-subvec-catvec
+  ;; Consider replacing with extra-debug-checks versions of these
+  ;; functions later, e.g. what I call dv/dbg-catvec and dv/dbg-subvec
+  ;; on a branch.
+  (let [my-catvec fv/catvec
+        my-subvec fv/subvec]
+    (letfn [(insert-by-sub-catvec [v n]
+              (my-catvec (my-subvec v 0 n) (fv/vec ['x])
+                         (my-subvec v n)))
+            (repeated-subvec-catvec [i]
+              (reduce insert-by-sub-catvec (vec (range i)) (range i 0 -1)))]
+      (is (= (repeated-subvec-catvec 2371)
+             (interleave (range 2371) (repeat 'x)))))))
+
+(deftest test-reduce-subvec-catvec2
+  (let [my-catvec fv/catvec
+        my-subvec fv/subvec]
+    (letfn [(insert-by-sub-catvec [v n]
+              (my-catvec (my-subvec v 0 n) (fv/vec ['x])
+                         (my-subvec v n)))
+            (repeated-subvec-catvec [i]
+              (reduce insert-by-sub-catvec
+                      (vec (range i))
+                      (take i (interleave (range (quot i 2) ##Inf)
+                                          (range (quot i 2) ##Inf)))))]
+      (let [n 2371
+            v (repeated-subvec-catvec n)]
+        (is (every? #(or (integer? %) (= 'x %)) v))
+        (is (= (count v) (* 2 n)))))))
+
+(deftest test-splice-high-subtree-branch-count
+  (let [my-catvec fv/catvec
+        my-subvec fv/subvec
+        x        (fv/vec (repeat 1145 \a))
+        y        (my-catvec (my-subvec x 0 778) (my-subvec x 778 779) [1] (my-subvec x 779))
+        z        (my-catvec (my-subvec y 0 780) [2] (my-subvec y 780 781) (my-subvec y 781))
+        res      (my-catvec (my-subvec z 0 780) [] [3] (my-subvec z 781))
+        expected (concat (repeat 779 \a) [1] [3] (repeat 366 \a))]
+    (is (= res expected))))
+
+
+(defn npe-for-1025-then-pop! [kind]
+  (let [bfactor-squared (* 32 32)
+        mk-vector (case kind
+                    :object-array fv/vector
+                    (:long-array #(fv/vector-of :long)))
+        boundary 54
+        v1 (-> (mk-vector)
+               (into (range boundary))
+               (into (range boundary (inc bfactor-squared))))
+        v2 (-> (mk-vector)
+               (into (range bfactor-squared))
+               (transient)
+               (pop!)
+               (persistent!))
+        v3 (-> (mk-vector)
+               (into (range boundary))
+               (into (range boundary (inc bfactor-squared)))
+               (transient)
+               (pop!)
+               (persistent!))
+        v4 (-> (mk-vector)
+               (into (range (inc bfactor-squared)))
+               (transient)
+               (pop!)
+               (persistent!))]
+    ;; This test passes
+    (is (= (seq v1) (range (inc bfactor-squared))))
+    ;; This also passes
+    (is (= (seq v2) (range (dec bfactor-squared))))
+    ;; This fails with NullPointerException while traversing the seq
+    ;; on clj.  It gets a different kind of error with cljs.
+    (is (= (seq v3) (range bfactor-squared)))
+    ;; This one causes a NullPointerException while traversing the seq
+    (is (= (seq v4) (range bfactor-squared)))))
+
+;; TBD: Uncomment this test after I figure out which of my fixes gets
+;; it working again.
+#_(deftest test-npe-for-1025-then-pop!
+  (doseq [kind [:object-array :long-array]]
+    (npe-for-1025-then-pop! kind)))
 
 ;; This problem reproduction code is slightly modified from a version
 ;; provided in a comment by Mike Fikes on 2018-Dec-09 for this issue:
