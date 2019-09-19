d2e25a0edc2ea35d3ce95f9aa51c01413a053184
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-09-19 16:48:16 -0700

    Make collection-check tests a bit longer They are not run on build.clojure.org on every commit, only at explicit request on a developer's machine, so we may as well make them a bit longer.

--- src/test_local/clojure/clojure/core/rrb_vector_check.clj
+++ src/test_local/clojure/clojure/core/rrb_vector_check.clj
@@ -4,7 +4,21 @@
             [collection-check.core :refer [assert-vector-like]])
   (:use clojure.test))
 
+;; On my 2015 MacBook Pro with JDK 11, a few num-tests values and
+;; approximate run time of assert-vector-like test on Clojure
+;; implementation:
+
+;;  1,000:  16 sec
+;; 10,000: 120 sec
+
+(def medium-num-tests 10000)
+(def short-num-tests 1000)
+;;(def num-tests short-num-tests)
+(def num-tests medium-num-tests)
+
 (deftest collection-check
-  (assert-vector-like 250 (fv/vector) gen/int)
+  (println "Before assert-vector-like with num-tests=" num-tests)
+  (assert-vector-like num-tests (fv/vector) gen/int)
+  (println "After assert-vector-like with num-tests=" num-tests)
   (is (every? nil? (.-array ^clojure.lang.PersistentVector$Node
                             (.-root ^clojure.lang.PersistentVector (vector))))))
