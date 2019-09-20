0f0806f6155c7c432bc6375eef9934b724023112
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-09-20 14:17:32 -0700

    Add some comments of other parameter values

--- src/parameterized/clojure/clojure/core/rrb_vector/parameters.clj
+++ src/parameterized/clojure/clojure/core/rrb_vector/parameters.clj
@@ -1,26 +1,29 @@
 (ns clojure.core.rrb-vector.parameters)
 
 ;; The values in comments before each def are the value of that
-;; parameter when the shift-increment is 5, followed by its value when
-;; the shift-increment is 2.
+;; parameter:
 
-;; 5 2
+;; * when the shift-increment is 5
+;; * when the shift-increment is 3
+;; * when the shift-increment is 2
+
+;; 5 3 2
 (def shift-increment 5)
 
-;; 32 4
+;; 32 8 4
 (def max-branches (bit-shift-left 1 shift-increment))
 
-;; 0x1f 0x3
+;; 0x1f 0x7 0x3
 (def branch-mask (dec max-branches))
 
-;; 31 3
+;; 31 7 3
 (def max-branches-minus-1 (dec max-branches))
 
-;; 30 2
+;; 30 6 2
 (def max-branches-minus-2 (- max-branches 2))
 
-;; 33 5
+;; 33 9 5
 (def non-regular-array-len (inc max-branches))
 
-;; 1024 16
+;; 1024 64 16
 (def max-branches-squared (* max-branches max-branches))
