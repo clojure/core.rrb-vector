07f017c1043cd3aee47bcc3513e33c6a2925ca7f
Alex Miller <alex.miller@cognitect.com>
2020-09-16 12:37:05 -0500

    add copyright statements to src

--- src/main/cljs/clojure/core/rrb_vector/debug_platform_dependent.cljs
+++ src/main/cljs/clojure/core/rrb_vector/debug_platform_dependent.cljs
@@ -1,3 +1,11 @@
+;   Copyright (c) Rich Hickey and contributors. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns clojure.core.rrb-vector.debug-platform-dependent
   (:require [clojure.core.rrb-vector.rrbt :as rrbt]
             [clojure.core.rrb-vector.nodes :refer [regular? node-ranges]]
